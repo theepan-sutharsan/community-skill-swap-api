e9acc26ce415ad9f2c4cc5b5c2450fb27b9c9f10
Kamsika <kamsika0401@gmail.com>
2026-06-29 12:42:18 +0530

    skill_model.py  skill_id updated

--- app/models/skills_model.py
+++ app/models/skills_model.py
@@ -5,7 +5,7 @@ from app.utils import utc_now
 class Skill(db.Model):
     __tablename__ = "skills"
 
-  
+    Skill_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Skill_name = db.Column(db.String(100), nullable=False)
     catogory = db.Column(db.String(120), nullable=False)
    
