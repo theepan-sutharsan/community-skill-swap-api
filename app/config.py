b0e454a9babd45bd0eaa6cd18247c4ae1cfed2eb
sutharsan <theepansutharsan1117@gmail.com>
2026-06-29 12:14:04 +0530

    config.py updated

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config:
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "root123")
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_NAME = os.getenv("DB_NAME", "student_db")
+    DB_NAME = os.getenv("DB_NAME", "community-skill-swap-db")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
