ff3e99ff27cd874a959a806f7a06e8c8d3a7f420
Kamsika <kamsika0401@gmail.com>
2026-06-29 12:18:12 +0530

    config.py file updated

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -10,6 +10,9 @@ class User(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     age = db.Column(db.Integer, nullable=False)
     cgpa = db.Column(db.Float, default=0.0)
+    is_active = db.Column(db.Boolean, default=True)
+    joined_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, default=utc_now)
 
 
 
