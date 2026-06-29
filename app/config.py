37f420cde95124a71716f579bd130c3a2693d9be
sutharsan <theepansutharsan1117@gmail.com>
2026-06-29 12:13:19 +0530

    config.py updated

--- app/config.py
+++ app/config.py
@@ -1,22 +1,24 @@
 from datetime import timedelta
-import os 
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class config:
-    DB_USER = os.getenv("DB_USER","root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD","root123")
-    DB_HOST = os.getenv("DB_HOST","localhost")
-    DB_NAME = os.getenv("DB_NAME","community-skill-swap_db")
 
+class Config:
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root123")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("DB_NAME", "student_db")
 
-    SQLALCHEMY_DATABASE_URI=(
+    SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
-
-    SQLALCHEMY_TRACK_MODIFICATION = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY","super-secret-key-change-me")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key-change-me")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
         minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES", "15"))
     )
+
+
