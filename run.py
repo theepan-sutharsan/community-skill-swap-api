bc2401ba843dac59bdc502133bc94d8dcb10888f
sutharsan <theepansutharsan1117@gmail.com>
2026-06-30 13:28:35 +0530

    run.py updated

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from flask_cors import CORS
-
 from app import create_app
+from app.extensions import db
 
-app= create_app()
+from app.models import user_model, skill_model, user_skill_model
+from app.models import swap_request_model, session_model
+
+app = create_app()
 CORS(app)
 
 with app.app_context():
-    from app.extensions import db
     db.create_all()
 
-if __name__ =="__main__":
-   app.run(debug=True,port=5000)     
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
