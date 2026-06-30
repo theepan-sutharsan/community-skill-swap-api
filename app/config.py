d62e6b8adf4f6e94906250e3bdf92da0d26626f2
sutharsan <theepansutharsan1117@gmail.com>
2026-06-30 14:12:12 +0530

    config.py updated

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root123")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_NAME = os.getenv("DB_NAME", "skill_db")
 
